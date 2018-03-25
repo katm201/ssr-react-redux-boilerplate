ae715807b710eb6b391eb7ab6745cfaaf7d77a8f
Katherine Manning <katm201@gmail.com>
2018-03-25 15:34:53 -0700

    [setup] create html template to fetch bundle.js when loaded

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 
+import template from './html-template';
 import Home from './client/components/Home';
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
   const content = renderToString(<Home />);
-
-  response.send(content);
+  const html = template(content);
+  response.send(html);
 });
 
 app.listen(8080, () => {

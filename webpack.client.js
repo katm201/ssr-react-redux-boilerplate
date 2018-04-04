99492fc5f452c1863e47a1140d373068daaabf85
Katherine Manning <katm201@gmail.com>
2018-04-03 18:43:42 -0700

    [setup] add css loaders to webpack client file

--- webpack.client.js
+++ webpack.client.js
@@ -9,6 +9,15 @@ const config = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+        include: path.join(__dirname, 'src/client/assets/stylesheets/stylesheet.css'),
+      },
+    ],
+  },
 };
 
 module.exports = merge(base, config);
